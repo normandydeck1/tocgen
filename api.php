aea7d03d8c52cb4c204f40332bdff50c1144ea3d
h.ruhs <henry.ruhs@unister.de>
2013-11-29 18:35:55 +0100

    Introducing API support

---
+++ api.php
@@ -0,0 +1,40 @@
+<?php
+error_reporting(0);
+$currentDirectory = dirname(__FILE__);
+
+/* include files */
+
+include_once($currentDirectory . '/tocgen.php');
+
+/* define variables */
+
+$target = $_POST[1];
+$config = $_POST[2];
+
+$targetFile = $currentDirectory . '/temp/' . sha1($target) . '.tmp';
+$configFile = $currentDirectory . '/temp/' . sha1($config) . '.tmp';
+
+$argv = array(
+    1 => $targetFile,
+    2 => $configFile
+);
+
+/* put files */
+
+file_put_contents($targetFile, $target);
+file_put_contents($configFile, $config);
+
+/* process */
+
+$tocgen = new Tocgen($argv, $currentDirectory);
+$tocgen->process();
+
+/* get contents */
+
+echo file_get_contents($targetFile);
+
+/* unlink files */
+
+unlink($targetFile);
+unlink($configFile);
+?>
